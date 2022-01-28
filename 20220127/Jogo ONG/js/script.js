38e8a286b09a20935cacebb6f140cb0f0731e549
Rodrigo <guigo.xd@live.com>
2022-01-27 22:09:41 -0300

    Exercicio Ong e Média Kms

---
+++ 20220127/Jogo ONG/js/script.js
@@ -0,0 +1,7 @@
+let idade=parseInt(prompt("Digite a sua Idade"));
+if (idade>=12){
+    alert ("Você pode jogar!");
+}
+else{
+    alert ("Sua idade não permite que você jogue!");
+}
---
+++ 20220127/Media Combustivel/js/script.js
@@ -0,0 +1,6 @@
+let n1=parseFloat(prompt("Digite o KM inicial:"));
+let n2=parseFloat(prompt("Digite o KM parada posto gasolina:"));
+let n3=parseFloat(prompt("Digite a quantidade de Litros abastecidos"));
+let distancia=n2-n1
+let media=distancia/n3
+alert (`média ${media}`)
